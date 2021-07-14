c704675b924321f21b290cca162355d73876e54d
MelinaMamani <erikamelinamamani@gmail.com>
2021-07-13 22:23:30 -0300

    Variables y reacomodo carpeta JS

--- Javascript/hola-mundo/js/01-hola-mundo.js
+++ Javascript/hola-mundo/js/01-hola-mundo.js
@@ -1,4 +1,4 @@
-alert("Hola Mundo con JS");
-alert("Bienvenida a JS");
+/*alert("Hola Mundo con JS");
+alert("Bienvenida a JS");*/
 document.write("Hola mundo desde fichero externo");
 console.log("Muestro en consola");
---
+++ Javascript/hola-mundo/js/02-variables.js
@@ -0,0 +1,11 @@
+// Variables
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+pais = "Argentina";
+continente = "America";
+
+console.log(pais, continente, antiguedad);
+console.log(pais_y_continente);
